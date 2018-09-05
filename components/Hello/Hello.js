e1ec7338f4b24378ec07f158661a713361448070
Łukasz Borek <Borek_Lukasz@outlook.com>
2018-09-05 15:20:36 +0200

    Add arrow-function

---
+++ components/Hello/Hello.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Hello = (props, context) => {
+  return (
+      <div>
+            <h1>Hello World!</h1>
+      </div>
+  );
+}
+
+Hello.propTypes = {
+};
+
+export default Hello;
